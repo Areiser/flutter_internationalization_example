09cec2c1c81febd88e9562ba9bd215a313b139c5
Andreas Reiser <areiser@hm.edu>
2017-10-04 19:18:34 +0200

    Add localization file

---
+++ lib/my_localizations.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+
+class MyLocalizations {
+  MyLocalizations(Locale locale) {
+    _nameToValue = _localeToNameToValue[locale.languageCode];
+  }
+
+  Map<String, String> _nameToValue;
+  Map<String, Map<String, String>> _localeToNameToValue = {
+    'en': <String, String>{               // English
+      'helloWorld': 'Hello World',
+    },
+    'fr': <String, String>{               // French
+      'helloWorld': 'Bonjour le monde',
+    },
+    // ...
+  };
+
+  static Future<MyLocalizations> load(Locale locale) {
+    return new SynchronousFuture<MyLocalizations>(new MyLocalizations(locale));
+  }
+
+  static MyLocalizations of(BuildContext context) {
+    return Localizations.of<MyLocalizations>(context, MyLocalizations);
+  }
+
+  String get helloWorld => _nameToValue['helloWorld'];
+// ... more methods like get helloWorld
+}
+
+
